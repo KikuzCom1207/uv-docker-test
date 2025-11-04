682ee6b3f366199c69cec7073ea7b00925c14f9f
KikuzCom1207 <231205075@ccmailg.meijo-u.ac.jp>
2025-11-04 13:56:09 +0900

    hotfix: docker build

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@
 # The `docker run` command uses the following options:
 #
 #   --rm                        Remove the container after exiting
-#   --volume .:/app             Mount the current directory to `/app` so code changes don't require an image rebuild
-#   --volume /app/.venv         Mount the virtual environment separately, so the developer's environment doesn't end up in the container
+#   --volume .:/workspace       Mount the current directory to `/workspace` so code changes don't require an image rebuild
+#   --volume /workspace/.venv   Mount the virtual environment separately, so the developer's environment doesn't end up in the container
 #   --publish 8000:8000         Expose the web server port 8000 to the host
 #   -it $(docker build -q .)    Build the image, then use it as a run target
-#   $@                          Pass any arguments to the container
+#   $@                          Pass any arguments to the container (prefixed with 'uv run' if arguments provided)
 
 if [ -t 1 ]; then
     INTERACTIVE="-it"
@@ -19,11 +19,16 @@ else
     INTERACTIVE=""
 fi
 
+# If arguments are provided, prefix them with 'uv run' to execute project scripts
+if [ $# -gt 0 ]; then
+    set -- uv run "$@"
+fi
+
 docker run \
     --rm \
-    --volume .:/app \
-    --volume /app/.venv \
+    --volume .:/workspace \
+    --volume /workspace/.venv \
     --publish 8000:8000 \
     $INTERACTIVE \
-    $(docker build -q .) \
+    $(docker build -q -f docker/Dockerfile .) \
     "$@"
